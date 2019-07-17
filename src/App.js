099428cc911c959c50685b18a218e973e541c122
dcmarin91 <dcmarin91@hotmail.com>
2019-07-16 21:03:34 -0500

    Fix word repiter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.copy = this.copy.bind(this);
+    this.state = {value: ""}
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.copy} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.value}</p>
       </div>
     );
+  }
+  copy(ev){
+    this.setState({value: ev.target.value})
   }
 }
 
